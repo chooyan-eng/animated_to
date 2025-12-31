14ffab9bd72b317177cb38779290529e5315f16a
chooyan-eng <chooyan.engineer@gmail.com>
2025-12-31 16:37:06 +0900

    fix: listview sample page

--- example/lib/listview_page.dart
+++ example/lib/listview_page.dart
@@ -33,52 +33,54 @@ class _ListPageState extends State<ListPage> with TickerProviderStateMixin {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // Set dark theme colors
-      backgroundColor: Colors.grey[900],
-      appBar: AppBar(
-        foregroundColor: Colors.white,
-        backgroundColor: Colors.grey[850],
-        title: const Text('ListView Page'),
-      ),
-      body: ListView.builder(
-        controller: _controller,
-        itemCount: items.length,
-        // controller: _controller,
-        itemBuilder: (context, index) => Padding(
-          padding: const EdgeInsets.all(8.0),
-          child: AnimatedTo.spring(
-            verticalController: _controller,
-            slidingFrom: Offset(200 + index * 10, 0),
-            globalKey: keys[index],
-            child: ListTile(
-              onTap: () {
-                setState(() {
-                  _currentSelected = index;
-                });
-              },
-              shape: RoundedRectangleBorder(
-                borderRadius: BorderRadius.circular(8),
-              ),
-              tileColor: Colors.grey[850],
-              contentPadding: const EdgeInsets.all(16),
-              leading: index == _currentSelected
-                  ? AnimatedTo.spring(
-                      globalKey: _iconKey,
-                      verticalController: _controller,
-                      child: const Icon(
-                        Icons.folder,
-                        color: Colors.blue,
-                        size: 28,
-                      ),
-                    )
-                  : null,
-              title: Text(
-                items[index],
-                style: const TextStyle(
-                  color: Colors.white,
-                  fontSize: 16,
-                  fontWeight: FontWeight.w500,
+    return AnimatedToBoundary(
+      child: Scaffold(
+        // Set dark theme colors
+        backgroundColor: Colors.grey[900],
+        appBar: AppBar(
+          foregroundColor: Colors.white,
+          backgroundColor: Colors.grey[850],
+          title: const Text('ListView Page'),
+        ),
+        body: ListView.builder(
+          controller: _controller,
+          itemCount: items.length,
+          // controller: _controller,
+          itemBuilder: (context, index) => Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: AnimatedTo.spring(
+              verticalController: _controller,
+              slidingFrom: Offset(200 + index * 10, 0),
+              globalKey: keys[index],
+              child: ListTile(
+                onTap: () {
+                  setState(() {
+                    _currentSelected = index;
+                  });
+                },
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.circular(8),
+                ),
+                tileColor: Colors.grey[850],
+                contentPadding: const EdgeInsets.all(16),
+                leading: index == _currentSelected
+                    ? AnimatedTo.spring(
+                        globalKey: _iconKey,
+                        verticalController: _controller,
+                        child: const Icon(
+                          Icons.folder,
+                          color: Colors.blue,
+                          size: 28,
+                        ),
+                      )
+                    : null,
+                title: Text(
+                  items[index],
+                  style: const TextStyle(
+                    color: Colors.white,
+                    fontSize: 16,
+                    fontWeight: FontWeight.w500,
+                  ),
                 ),
               ),
             ),
